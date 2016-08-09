75c2f6d542a7e9523206a62e084b7e6c685193d4
mackmoe <maurice.nash1@gmail.com>
2016-08-09 05:22:40 -0500

    updated script
    Was using the wrong EP to upload container

--- image-imp-and-exp.sh
+++ image-imp-and-exp.sh
@@ -12,48 +12,50 @@ echo "Enter the name of the exsisting container you're exporting to:";read EXP_C
 echo "Enter the name of the exsisting container you're importing to:";read IMP_CONTAINER;
 echo "Enter the name of what you'd like to call the imported image:";read IMAGENAME;
 
-####### Basic Authentication This section simply retrieves the TOKEN 
+####### Basic Authentication This section simply retrieves the TOKEN
 TOKEN=`curl -s https://identity.api.rackspacecloud.com/v2.0/tokens -X POST \
 -d '{ "auth":{"RAX-KSKEY:apiKeyCredentials": { "username":"'$USERNAME'", "apiKey": "'$APIKEY'" }} }' \
 -H "Content-type: application/json" |  python -mjson.tool | grep -A5 token | grep id | cut -d '"' -f4`
- 
+
 ####### This section requests the Glance API to copy the cloud server image uuid to a cloud files container and stores the task id
 EXP_TASKID=`curl -s "https://$EXP_DC.images.api.rackspacecloud.com/v2/$CUSTOMERID/tasks" -X POST \
 -d '{"type": "export", "input": {"image_uuid": "'"$IMAGEID"'", "receiving_swift_container": "'$EXP_CONTAINER'"}}' \
 -H "X-Auth-Token: $TOKEN" -H "Content-Type: application/json" | python -mjson.tool | grep [i]d | grep -v "$IMAGEID"  | cut -d '"' -f4`
-echo "The export task ID is '$EXP_TASKID'"
+echo "The export task ID is $EXP_TASKID"
 
 ####### This section is a function get's the export status
 getEXPprogress() {
   EXP_POLL=$(curl -s https://$EXP_DC.images.api.rackspacecloud.com/v2/$CUSTOMERID/tasks/$EXP_TASKID -X GET -H "X-Auth-Token: $TOKEN" -H "Content-Type: application/json")
   EXP_STATUS=$(echo $EXP_POLL | python -m json.tool | grep '"status": ' | sed -e 's/ //g' -e 's/"//g' -e 's/,//g' | cut -d: -f2)
 }
+
+####### while loop to poll export until complete
 getEXPprogress
-while [[ $EXP_STATUS != "success" ]]; do
+while [[ $EXP_STATUS != success ]]; do
   echo "Image export is: $EXP_STATUS"
   getEXPprogress
   sleep 60
 done
 
 ####### This section calls the Cloud Files API, asks for the vhd file and places it into a variable
-CF_EP=$(curl -s https://identity.api.rackspacecloud.com/v2.0/tokens -X POST -d '{"auth":{"RAX-KSKEY:apiKeyCredentials":{"username":"mackmoe210", "apiKey":"0620342ba8624859ab9e3467ed6b923b"}}}' -H "Content-Type: application/json" | python -m json.tool | grep -A27 '"type": "object-store"' | egrep -i "region|publicURL" | cut -d '"' -f4 | grep -i -B1 $EXP_DC | sed -n '/https/p')
-IMG_VHD=$(curl -s $CF_EP/$EXP_CONTAINER -H "X-Auth-Token: $TOKEN")
+CF_EP=$(curl -s https://identity.api.rackspacecloud.com/v2.0/tokens -X POST -d '{"auth":{"RAX-KSKEY:apiKeyCredentials":{"username":"'$USERNAME'", "apiKey":"'$APIKEY'"}}}' -H "Content-Type: application/json" | python -m json.tool |grep -A30 '"serviceCatalog"' | egrep -i "region|publicURL" | cut -d '"' -f4 | grep -i -B1 "$EXP_DC" | sed -n '/https/p')
+IMG_VHD=$(curl -s $CF_EP/$EXP_CONTAINER/$IMAGEID.vhd -H "X-Auth-Token: $TOKEN")
 
 ####### This section requests the Glance API to import the cloud server image to the specified container and stores the task id
 IMP_TASKID=`curl -s "https://$EXP_DC.images.api.rackspacecloud.com/v2/$CUSTOMERID/tasks" -X POST \
 -d '{"type": "import", "input": {"image_properties": "'"$IMAGENAME"'", "import_from": "'$IMG_VHD'"}}' \
 -H "X-Auth-Token: $TOKEN" -H "Content-Type: application/json" | python -mjson.tool | grep [i]d | grep -v "$IMAGEID"  | cut -d '"' -f4`
-echo "The import task ID is '$IMP_TASKID'"
+echo "The import task ID is $IMP_TASKID"
 
 ####### This section is a function get's the import status - then  reports back once it's complete
 getIMPprogress() {
-  IMP_POLL=$(curl https://$EXP_DC.images.api.rackspacecloud.com/v2/$CUSTOMERID/tasks/$IMP_TASKID -X GET \
-  -H "X-Auth-Token: $TOKEN" \
-  -H "Content-Type: application/json")
+  IMP_POLL=$(curl -s "https://$EXP_DC.images.api.rackspacecloud.com/v2/$CUSTOMERID/tasks/$IMP_TASKID" -X GET -H "X-Auth-Token: $TOKEN" -H "Content-Type: application/json")
   IMP_STATUS=$(echo $IMP_POLL | python -m json.tool | grep '"status": ' | sed -e 's/ //g' -e 's/"//g' -e 's/,//g' | cut -d: -f2)
 }
+
+####### while loop to poll import until complete
 getIMPprogress
-while [[ $IMP_STATUS != "success" ]]; do
+while [[ $IMP_STATUS != success ]]; do
   echo "Image export is: $IMP_STATUS"
   getIMPprogress
   sleep 60
